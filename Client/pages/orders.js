abaa71bd57f6fd4f0c0138965b0e0d24041680da
Dameon Mendoza <dameonmendoza@gmail.com>
2021-02-07 12:59:53 -0700

    module 5

--- Client/pages/orders.js
+++ Client/pages/orders.js
@@ -1,6 +1,5 @@
 export default function OrdersPage() {
-  return 
-          <h1>
+  return <h1>
            Hello Order World
           </h1>
          
--- Client/pages/products.js
+++ Client/pages/products.js
@@ -3,5 +3,4 @@ export default function ProductsPage() {
   return <h1>
           Hello Products Page
          </h1>
-          
 }
