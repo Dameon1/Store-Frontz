1a2737eaff9bf97dea0f084e435357a14e7a5976
Dameon Mendoza <dameonmendoza@gmail.com>
2021-02-08 12:36:26 -0700

    modules 6-9

--- Client/components/Header.js
+++ Client/components/Header.js
@@ -1,14 +1,54 @@
 import Link from "next/link";
 import Nav from "./Nav";
 
+import styled from 'styled-components';
+
+const Logo = styled.h1`
+  font-size: 4rem;
+  margin-left: 2rem;
+  position: relative;
+  z-index: 2;
+  background: green;
+  transform: skew(-7deg);
+  a {
+    color: white;
+    text-decoration: none;
+    text-transform: uppercase;
+    padding: 0.5rem 1rem;
+  }
+`;
+
+const HeaderStyles = styled.header`
+  .bar {
+    border-bottom: 10px solid black;
+    display: grid;
+    grid-template-columns: auto 1fr;
+    justify-content: space-between;
+    align-items: stretch;
+  }
+
+  .sub-bar {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    border-bottom: 1px solid var(--black, black);
+  }
+`;
+
 export default function Header() {
-  return <header>
-     <div className="bar">
-       <Link href="/">Store Frontz</Link>
-     </div>
-     <div className="sub-bar">
-      <p>Search</p>
-     </div>
-     <Nav />
-    </header>
+
+  return (
+          <HeaderStyles>
+            <div className="bar">
+              <Logo>
+                <Link href="/">
+                  Store Frontz
+                </Link>
+              </Logo>
+            </div>
+            <div className="sub-bar">
+              <p>Search</p>
+            </div>
+            <Nav />
+          </HeaderStyles>
+  )
 }
