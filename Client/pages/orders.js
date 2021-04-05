73ea17a1ef5d7feb14857ab4fb204e5219f43576
Dameon Mendoza <dameonmendoza@gmail.com>
2021-04-05 08:22:09 -0700

    Pagination Dynamic Routing

--- Client/pages/orders.js
+++ Client/pages/orders.js
@@ -1,9 +1,12 @@
+import { useRouter } from 'next/dist/client/router';
 import Products from '../components/Products';
 import Pagination from '../components/Pagination';
 
 
 
 export default function OrderPage() {
+  const router = useRouter();
+  console.log(router)
   return (
     <>
       <Pagination page={1}/>
