fceb45cee1a7663e70261874ee7dd52c89c1eddd
Dameon Mendoza <dameonmendoza@gmail.com>
2021-04-10 06:20:20 -0700

    Querying the Current User

--- Client/components/Nav.js
+++ Client/components/Nav.js
@@ -1,12 +1,20 @@
 import Link from "next/link";
 import NavStyles from './styles/NavStyles';
+import { useUser } from "./User";
+
 
 export default function Nav() {
-
-  return <NavStyles>
-    <Link href ="/sell">Sell</Link>
-    <Link href ="/orders">Orders</Link>
-    <Link href ="/products">Products</Link>
-    <Link href ="/accounts">Account</Link>
-  </NavStyles>
+  const user = useUser();
+  console.log(user)
+  return (
+    <NavStyles>
+      { !user && (<Link href ="/signin">Sign In</Link>)}
+      <Link href ="/sell">Sell</Link>
+      { user && ( <>
+                    <Link href ="/orders">Orders</Link>
+                    <Link href ="/products">Products</Link>
+                    <Link href ="/accounts">Account</Link>
+                  </> )}
+    </NavStyles>
+  )
 }
